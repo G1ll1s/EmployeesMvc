f300e65d9d7c8e902cc8cb0e154066d91349bc3b
G1ll1s <Gillis_1987@hotmail.com>
2024-11-05 13:35:09 +0100

    sgf

---
+++ EmployeesMvc/Models/DataService.cs
@@ -0,0 +1,31 @@
+﻿namespace EmployeesMvc.Models
+{
+    public class DataService
+    {
+        private static List<Employee> employees = [
+            new Employee { Id = 1, Name = "Oscar", Email = "Test@gmail.com" },
+            new Employee { Id = 2, Name = "Juan", Email = "Juan@hotmail.com" },
+            new Employee { Id = 2, Name = "Jonas", Email = "Jonas@hotmail.com" },
+            ];
+        int nextId = 0;
+
+        public Employee[] GetAllEmployees()
+        {
+            return employees
+                .OrderBy(e => e.Name)
+                .ThenBy(e => e.Email)
+                .ToArray();
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return employees.SingleOrDefault(e => e.Id == id);
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            employee.Id = nextId++;
+            employees.Add(employee);
+        }
+    }
+}
---
+++ EmployeesMvc/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesMvc.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter a Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter an E-mail")]
+        [EmailAddress(ErrorMessage = "Enter a valid E-mail")]
+        [Display(Name ="E-mail")]
+        public string Email { get; set; }
+    }
+}
